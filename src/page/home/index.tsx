49936cddf53b3c93a123c4fabd6213cb6e1f487e
Vandson <vandsonsf@gmail.com>
2021-04-19 20:26:22 -0300

    creating and setup project

---
+++ src/page/home/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function home() {
+  return <Container>teste</Container>;
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,72 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    :root {
+        --background: #fff;
+        --shape: #f0f2f5;
+        --text: #71717a;
+        --green: #22c55e;
+        --blue: #2563eb;
+        --dark-blue: #1e3a8a;
+    }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html{ 
+        @media (max-width: 1080px){
+            font-size: 93.75%;
+        }
+        @media (max-width: 720px){
+            font-size: 87.5%;
+        }
+    }
+
+    body{
+        background: var(--background);
+        --webkit-font-smoothing: antialiased; 
+    }
+    body, input, textarea, button{
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    input[type="number"] {
+        -moz-appearance: textfield;
+        appearance: textfield;
+    }input[type=number]::-webkit-inner-spin-button { 
+    -webkit-appearance: none;
+    }
+    h1, h2, h3, h4, h5, strong {
+        font-weight: 600;
+    }
+    button { 
+        cursor: pointer;
+    }
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+    
+    // ANIMATIONS
+    @keyframes appearing-from-right {
+        0% {
+            transform: translate(5vw,0);
+            opacity: 0;
+        }
+        100% {
+            transform: translate(0,0);
+            opacity: 1;
+        }
+    }
+    @keyframes appearing-from-left {
+        0% {
+            transform: translate(-5vw,0);
+            opacity: 0;
+        }
+        100% {
+            transform: translate(0,0);
+            opacity: 1;
+        }
+    }
+`; 
